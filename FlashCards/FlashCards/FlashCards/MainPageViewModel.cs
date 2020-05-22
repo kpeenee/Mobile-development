dd410421a8351b0be908345d279e38976e2ccf2f
imfizh <43548110+imfizh@users.noreply.github.com>
2020-05-22 13:40:04 +0100

    Added delete and edit function
    Added delete function for single flashcards and groups of flashcards, as well as an edit function

--- FlashCards/FlashCards/FlashCards/MainPageViewModel.cs
+++ FlashCards/FlashCards/FlashCards/MainPageViewModel.cs
@@ -16,5 +16,28 @@ namespace FlashCards
         {
             flashcards = SaveSystem.Load();
         }
+        public void RemoveFlashcard(int i)
+        {
+            flashcards.RemoveAt(i);
+            SaveSystem.Save(flashcards);
+        }
+        public void RemoveFlashcardGroup(string topic)
+        {
+            foreach (var flash in flashcards)
+            {
+                if (flash.Topic == topic)
+                {
+                    flashcards.Remove(flash);
+                }
+            }
+            SaveSystem.Save(flashcards);
+        }
+        public void EditFlashcard(int index, Flashcard editFlashcard)
+        {
+            flashcards[index].Topic = editFlashcard.Topic;
+            flashcards[index].Question = editFlashcard.Question;
+            flashcards[index].Answer = editFlashcard.Answer;
+            SaveSystem.Save(flashcards);
+        }
     }
 }
